2aaaa69db8d9b9f7e1c9f86c1966b3992f0bf3cb
khalid hesham <104100832+Khalid-Hesham@users.noreply.github.com>
2023-09-04 12:25:27 +0300

    Updating CRC

--- CRC.v
+++ CRC.v
@@ -19,16 +19,18 @@ output  reg             Valid					// high during output transmission, low otherw
 // Internal signals and registers
 reg [LFSR_WD-1:0] LFSR;			//LFSR register declaration
 reg [DATA_WD-1:0] Counter;		//to count the input data bits
-reg  counter_done;				//flag activited when the counter counts all the input data bits
-wire Feedback;					//feedback declared as a wire in the LFSR implementation 
+wire  counter_done;			//flag activited when the counter counts all the input data bits
+wire Feedback;				//feedback declared as a wire in the LFSR implementation 
 
 //  Integers
 integer i;
 
 // Assign statments
 assign Feedback = LFSR[0] ^ DATA;
+assign counter_done = (Counter == DATA_WD);
 
-// Sequential always block
+
+// Always Block For CRC Operation
 always@(posedge CLK or negedge RST)
 begin
 	if (!RST) 
@@ -37,38 +39,44 @@ begin
 			CRC <= 1'b0;
 			Valid <= 1'b0;
 			Counter <= 'b0;
-			counter_done <= 1'b0;
 		end
-	else if(ACTIVE && !counter_done)
-		begin
-			Counter <= Counter + 1'b1;    	
+	else if(ACTIVE && !counter_done) // Entering data serially to the LFSR
+		begin  	
+			Counter <= Counter + 1'b1;
+			// Doing CRC operation 
 			for (i = 0; i < LFSR_WD-1; i=i+1) 			
-			begin
-				if (Taps[i])   	//Taps 0100_0100	
-					LFSR[i] <= LFSR[i+1] ^ Feedback;
-				else 
-					LFSR[i] <= LFSR[i+1];
-			end 
-			LFSR[7] <= Feedback ;   	 			 	 
+				begin
+					if (Taps[i])   	//Taps 0100_0100
+						LFSR[i] <= LFSR[i+1] ^ Feedback;
+					else 
+						LFSR[i] <= LFSR[i+1];
+				end 
+			LFSR[7] <= Feedback ;   
+			if(Counter == DATA_WD - 1)
+				Valid <= 1'b1;	 			 	 
 		end
-	else if(!ACTIVE && counter_done)
+	else if(!ACTIVE && counter_done) // Extracting data serially from the LFSR
 	    begin
 			CRC <=	LFSR[0];
-			LFSR <= LFSR >> 1; 
+			LFSR <= LFSR >> 1;
 		end
 end
 
-// Combinational always block
-always@(*)
-begin
-	if(Counter == DATA_WD)
-	begin
-		Counter = 'b0;
-		counter_done = 1'b1;
-		Valid = 1'b1;
-	end   
-end
 
-
+////////////////////////////////////////////////////
+///////////  Tracing the LFSR operation  ///////////
+////////////////////////////////////////////////////
+//LFSR -> 1 1 0 1 _ 1 0 0 0
+//DATA -> 93 -> 1001_0011
+//Feedback -> 0^1=1
+//LFSR[7]=1, LFSR[6]=0, LFSR[5]=1, LFSR[4]=0,
+//LFSR[3]=1, LFSR[2]=0, LFSR[1]=0, LFSR[0]=0 
+////////////////////////////////////////////////////
+//LFSR -> 1 1 0 1 _ 1 0 0 0
+//DATA -> 72 -> 0111_0010 
+//Feedback -> 0^0=0
+//LFSR[7]=0, LFSR[6]=1, LFSR[5]=1, LFSR[4]=0,
+//LFSR[3]=1, LFSR[2]=1, LFSR[1]=0, LFSR[0]=0
+////////////////////////////////////////////////////
 
 endmodule 
